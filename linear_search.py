3c4ff02f3075d3b5eec0a156cf92a63a924e7a0c
Arun Chary Kammari <78126887+arun-chary@users.noreply.github.com>
2025-03-23 15:28:56 +0530

    Update linear_search.py
    Linear search using iteration

--- linear_search.py
+++ linear_search.py
@@ -17,3 +17,24 @@ def linear_search(arr, target):
     if arr[0] == target:
         return True
     return linear_search(arr[1:], target)
+
+
+# Linear search using iteration
+def linear_search(arr, target):
+    """
+    Function to perform linear search on an array using recursion.
+    
+    Parameters:
+    arr (list of int): The array of integers.
+    target (int): The element to search for.
+    
+    Returns:
+    bool: True if target is found, False otherwise.
+    """
+    
+    for element in arr:
+        if element == target:
+            return True
+    else:
+        return False
+    
