f3aa5e0bd993405952dfaef336208cc61af51b70
Arun Chary Kammari <78126887+arun-chary@users.noreply.github.com>
2025-03-23 15:22:17 +0530

    Create linear_search.py
    Linear search program using recursion

---
+++ linear_search.py
@@ -0,0 +1,19 @@
+# Linear Search using recursion
+
+def linear_search(arr, target):
+    """
+    Function to perform linear search on an array using recursion.
+    
+    Parameters:
+    arr (list of int): The array of integers.
+    target (int): The element to search for.
+    
+    Returns:
+    bool: True if target is found, False otherwise.
+    """
+    if len(arr) == 0:
+        return False
+        
+    if arr[0] == target:
+        return True
+    return linear_search(arr[1:], target)
